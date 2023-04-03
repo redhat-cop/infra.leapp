c21a72cf43e44cb6dd53c80218a4a3d423faeb18
Mike Savage <mike.savage@gmail.com>
2023-04-03 17:34:47 -0400

    Adjust for pep8 E226 and W292

--- roles/snapshot/files/build_tags_dict.py
+++ roles/snapshot/files/build_tags_dict.py
@@ -8,6 +8,6 @@ tags_dict = ast.literal_eval(tags)
 
 dict_for_ec2_remote_facts = {}
 for key, value in tags_dict.items():
-    dict_for_ec2_remote_facts['tag:'+key] = value
+    dict_for_ec2_remote_facts['tag:' + key] = value
 
 print(dict_for_ec2_remote_facts)
