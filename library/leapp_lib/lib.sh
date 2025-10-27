d7dc2e6edb7c2034e9c3822459ecbc8f8ad85d18
Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
2025-10-27 14:29:49 +0100

    Add CI tests running in Testing Farm (#274)
    * Add CI tests running in Testing Farm
    
    * Add a .github/workflows/tft.yml workflow that runs test playbooks in
      Testing Farm
    * Add collection-requirements.yml to install required collections
    * Add meta/main.yml with meta info about the collection
    * Add a test plan plans/general.fmf
    * Add fmf tests in tests directory
    * Add test playbooks in tests/
    * Add tests/vars/common_upgrade_vars.yml with mapping of remediations
    
    Work in this commit has been done by tomasfratrik
    spetrosi takes the work over, I'll make further changes in future
    commits
    
    * Fixes by spetrosi
    
    * Move created library with downstream functions to leapp_lib
    * Update meta/main.yml
    * Fix syntax error in getting supported platforms
    * Fix compose name and library import
    * Fix how tests identify supported platforms
    * Add proper tests_default.yml playbooks
    * Move tmt tests to the tmt directory
    * Clean up test script, unset debug variable
    * Rename plan
    
    * Run all test playbooks
    
    * Run playbooks both for collection and for each role
    * Work with SR_EXCLUDED and _INCLUDED
    
    * Apply Tomas's feedback
    
    * Fix ansible-lint and shellcheck
    
    ---------
    
    Co-authored-by: Tomas Fratrik <tomasfratrik8@gmail.com>

---
+++ library/leapp_lib/lib.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   Description: Library for leapp Ansible collection tests
+#   Author: Sergei Petrosian <spetrosi@redhat.com>
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   library-prefix = leapp
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Copied from downstream tests/rhel-system-roles/Library/basic rolesGetAnsibleVersion
+leappGetAnsibleVersion() {
+    local ver
+    ver="$(ansible --version | grep '^ansible .*[0-9][.]')"
+    if [[ "$ver" =~ ^ansible\ ([0-9]+[.][0-9]+) ]]; then
+        echo "${BASH_REMATCH[1]}"
+    elif [[ "$ver" =~ ^ansible\ .*core\ ([0-9]+[.][0-9]+) ]]; then
+        echo "${BASH_REMATCH[1]}"
+    else
+        echo UNKNOWN_ANSIBLE_VERSION
+    fi
+}
+
+# Copied from downstream tests/rhel-system-roles/Library/basic library rolesInstallAnsible
+leappInstallAnsible() {
+    local ae_version
+    ae_version=$1
+    local ansible_pkg
+    local pkg_cmd
+    local baseurl
+
+    if rlIsRHELLike ">=8.6" && [ "$ANSIBLE_VER" != "2.9" ]; then
+        pkg_cmd="dnf"
+        ansible_pkg="ansible-core"
+    elif rlIsRHELLike 8; then
+        pkg_cmd="dnf"
+        ansible_pkg="ansible"
+        baseurl="https://rhsm-pulp.corp.redhat.com/content/dist/layered/rhel8/$(arch)/ansible/$ae_version/os/"
+    else
+        # el7
+        pkg_cmd="yum"
+        ansible_pkg="ansible"
+        if [ "$(arch)" == "ppc64le" ]; then
+            baseurl="https://rhsm-pulp.corp.redhat.com/content/dist/rhel/power-le/7/7Server/$(arch)/ansible/$ae_version/os/"
+        elif [ "$(arch)" == "s390x" ]; then
+            baseurl="https://rhsm-pulp.corp.redhat.com/content/dist/rhel/system-z/7/7Server/$(arch)/ansible/$ae_version/os/"
+        else
+            baseurl="https://rhsm-pulp.corp.redhat.com/content/dist/rhel/server/7/7Server/$(arch)/ansible/$ae_version/os/"
+        fi
+    fi
+
+    if rlIsRHELLike ">7"; then
+        if "$pkg_cmd" module info standard-test-roles > /dev/null 2>&1; then
+            "$pkg_cmd" -y module disable standard-test-roles
+        fi
+    fi
+    if [ -n "${baseurl:-}" ]; then
+        echo "[${ansible_pkg}-$ae_version]
+name=${ansible_pkg}-$ae_version
+baseurl=$baseurl
+enabled=1
+gpgcheck=0
+priority=1" > /etc/yum.repos.d/lsr-test-ansible.repo
+    fi
+
+    # We need to swap an ansible/ansible-core if other package has been previously installed. Otherwise try to install a chosen one.
+    action="install"
+    rpm --quiet -q ansible && test "$ansible_pkg" = "ansible-core" && action="swap ansible"
+    rpm --quiet -q ansible-core && test "$ansible_pkg" = "ansible" && action="swap ansible-core"
+
+    rlRun "$pkg_cmd -y $action $ansible_pkg"
+    rlAssertRpm "$ansible_pkg"
+}
+
+# declare -A leapp_test_playbooks
+
+leappGetTests() {
+    local -n test_playbooks=$1
+    local coll_path=$2
+    # local roles test_type_name type_test_playbooks_all type_test_playbooks_all
+
+    roles=("$coll_path"/roles/*)
+    for test_type in "$coll_path" "${roles[@]}"; do
+        test_type_name=$(basename "$test_type")
+        type_test_playbooks_all=$(
+            find "$test_type"/tests -maxdepth 1 -type f -name "tests_*.yml" | sort | xargs
+        )
+
+        if [[ -n $SR_ONLY_TESTS && $SR_ONLY_TESTS == *"$test_type_name"/* ]]; then
+            if [[ $SR_ONLY_TESTS == *"$test_type_name"/\** ]]; then
+                type_test_playbooks="$type_test_playbooks_all"
+            else
+                for test_playbook in $type_test_playbooks_all; do
+                    playbook_basename=$(basename "$test_playbook")
+                    if [[ $SR_ONLY_TESTS == *"$test_type_name"/"$playbook_basename"* ]]; then
+                        type_test_playbooks+=" $test_playbook"
+                    fi
+                done
+            fi
+        else
+            type_test_playbooks="$type_test_playbooks_all"
+        fi
+
+        test_playbooks["$test_type_name"]="$type_test_playbooks_all"
+    done
+
+    if [ -z "${test_playbooks[*]}" ]; then
+        rlDie "No test playbooks found"
+    fi
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#   Verification
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+#   This is a verification callback which will be called by
+#   rlImport after sourcing the library to make sure everything is
+#   all right. It makes sense to perform a basic sanity test and
+#   check that all required packages are installed. The function
+#   should return 0 only when the library is ready to serve.
+#
+#   This library does not do anything, it is only a list of functions, so simply returning 0
+leappLibraryLoaded() {
+    rlLog "Library loaded!"
+    return 0
+}
