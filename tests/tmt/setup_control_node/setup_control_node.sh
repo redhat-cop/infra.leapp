59c024ce0bd5efe2044699a0a80b0a467aab9436
Sergei Petrosian <spetrosi@redhat.com>
2026-09-17 12:37:42 +0200

    ci: Install collections using lsrAnsibleGalaxy that sets timeout to 300s
    Ansible Galaxy fails due to timeout, probably issues on Ansible server
    side.
    This change runs the ansible-galaxy using lsrAnsibleGalaxy wrapper from
    https://github.com/linux-system-roles/tft-tests/pull/107 that patches
    ansible-galaxy of any version to set timeout of 300s

--- tests/tmt/setup_control_node/setup_control_node.sh
+++ tests/tmt/setup_control_node/setup_control_node.sh
@@ -165,7 +165,9 @@ rlJournalStart
         rlRun "cat $repo_vars_file"
         leappDebugRepos
 
-        rlWaitForCmd "ansible-galaxy collection install -r $coll_path/meta/collection-requirements.yml -vv" -m 5
+        if ! lsrAnsibleGalaxy collection install -vv -r "$coll_path/meta/collection-requirements.yml"; then
+            rlDie "Failed to install dependencies from $req_file"
+        fi
 
         lsrSetAnsibleInjectFactVars "$SR_ANSIBLE_INJECT_FACT_VARS"
         lsrPrepareNodesInventories
