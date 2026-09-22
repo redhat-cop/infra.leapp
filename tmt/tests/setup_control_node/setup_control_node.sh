4d8aa5ca0d05744e68c1d0c5b5415fe0a6122395
mmacura311 <54061580+mmacura311@users.noreply.github.com>
2026-09-22 10:50:12 +0200

    Restructure tests (#434)
    * Move tmt content into separate directory
    
    * Move repository replacement code from plan metadata file to separate file
    
    Beside moving the script to a file following minor fixes were done
     - fix missing shebang
     - replace sed call with parameter expansion
     - add missing quoting
     - indentation
     - prevent leaking of values in case one of repositories is not set
    
    * Move leapp_lib under tmt directory
    
    * Bump timeout on ansible-galaxy dependency installation
    
    Raise timeout and fail the phase if the dependency installation fails.
    
    Co-authored-by: Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>
    
    * Add tmt_path parameter after tests moved to tmt dir
    
    ---------
    
    Co-authored-by: Sergei Petrosian <30409084+spetrosi@users.noreply.github.com>

--- tmt/tests/setup_control_node/setup_control_node.sh
+++ tmt/tests/setup_control_node/setup_control_node.sh
@@ -165,7 +165,9 @@ rlJournalStart
         rlRun "cat $repo_vars_file"
         leappDebugRepos
 
-        rlWaitForCmd "ansible-galaxy collection install -r $coll_path/meta/collection-requirements.yml -vv" -m 5
+        if ! rlWaitForCmd "ansible-galaxy collection install -vv -r $coll_path/meta/collection-requirements.yml --timeout 600"; then
+            rlDie "Failed to install Ansible dependencies"
+        fi
 
         lsrSetAnsibleInjectFactVars "$SR_ANSIBLE_INJECT_FACT_VARS"
         lsrPrepareNodesInventories
